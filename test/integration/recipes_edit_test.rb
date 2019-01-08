2ca5c4f22f7624db3f8fe19ddb249bcfbfc9b2c1
haytham hamdy <haytham24493@gmail.com>
2019-01-09 00:50:24 +0200

    implement chef signup and tests

--- test/integration/recipes_edit_test.rb
+++ test/integration/recipes_edit_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class RecipesEditTest < ActionDispatch::IntegrationTest
   def setup
-    @chef = Chef.create(chefname: "haytham",email: "haytham@test.com")
+    @chef = Chef.create(chefname: "haytham",email: "haytham@test.com",
+                        password: "password", password_confirmation: "password")
     @recipe = Recipe.create(name: "vegetable saute",description: "great vegetable sautee,add vegetable and oil",chef: @chef)
 
   end
