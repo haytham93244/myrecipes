81187e08146ad0c6b8f45e8bf108ee9e560042fe
haytham hamdy <haytham24493@gmail.com>
2019-01-09 22:11:22 +0200

    Add chef's edit action with tests

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -8,7 +8,7 @@ class ChefsController < ApplicationController
         @chef = Chef.new(chef_params)
         if @chef.save
             flash[:success] = "Welcome #{@chef.chefname} to MyRecipes App!"
-            redirect_to chef_path(@chef)
+            redirect_to @chef
         else
             render 'new'
         end
@@ -18,6 +18,19 @@ class ChefsController < ApplicationController
         @chef = Chef.find(params[:id])
     end
 
+    def edit 
+        @chef = Chef.find(params[:id])
+    end
+
+    def update
+        @chef = Chef.find(params[:id])
+        if @chef.update(chef_params)
+            flash[:success] = "Your account was updated successfully"
+            redirect_to chef_path(@chef)
+        else
+            render 'edit'
+        end
+    end
 
     private 
 
--- app/models/chef.rb
+++ app/models/chef.rb
@@ -9,5 +9,5 @@ class Chef < ApplicationRecord
     
     has_many :recipes    
     has_secure_password  
-    validates :password, presence: true   ,length: {minimum: 6}    
+    validates :password, presence: true   ,length: {minimum: 6}  ,allow_nil: true 
 end
