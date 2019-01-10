cdaa80b0a3ea2697f43f0e4e4b4f59bfeef3d9e0
haytham hamdy <haytham24493@gmail.com>
2019-01-10 18:50:56 +0200

    Complete Authentication System

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,8 @@
 class RecipesController < ApplicationController
     
     before_action :set_recipe ,only: [:show,:edit,:update]
+    before_action :require_user, except: [:index,:show]
+    before_action :recipe_same_user ,only: [:edit,:update,:destroy]
 
     def index
         @recipes = Recipe.paginate(page: params[:page],per_page: 5)
@@ -16,7 +18,7 @@ class RecipesController < ApplicationController
 
     def create 
         @recipe = Recipe.new(recipe_params)
-        @recipe.chef = Chef.first
+        @recipe.chef = current_chef
         if @recipe.save
             flash[:success] = "Recipe was created successfully!"
             redirect_to recipe_path(@recipe)
@@ -53,5 +55,12 @@ class RecipesController < ApplicationController
 
     def recipe_params 
         params.require(:recipe).permit(:name,:description)
+    end
+
+    def recipe_same_user
+        if current_chef != @recipe.chef
+            flash[:danger] = "You can only edit or delete your own recipes"
+            redirect_to recipes_path
+        end
     end
 end
